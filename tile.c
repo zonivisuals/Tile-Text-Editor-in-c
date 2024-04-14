cbd6f1ad42b42eb96ae94f369863edea9a715470
Mohamed Hajji <mohamed.hajji.med@gmail.com>
2024-04-14 15:09:53 +0100

    7th error handling

--- tile.c
+++ tile.c
@@ -3,39 +3,50 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
+//exit the program whenever an error occurs
+void die(const char *s){
+	perror(s);
+	exit(0);
+}
+
 //Restore terminal attributes
 void disableRawMode(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1)
+		die("tcsetattr");
 }
 
 //RawMode: the terminal send each character it gets in it to the computer (computer keeps track of your moves <:) )
 void enableRawMode(){
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if(tcgetattr(STDIN_FILENO, &orig_termios)==-1) die("tcgetattr");
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
 	raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP |IXON | ICRNL); //cntrl+s & cntrl+q //ICRNL: cntrl+m & Enter
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); //ISIG: cntrl+c & cntrl+z //IEXTEN: cntrl+o & ctrl+v 
 	raw.c_cflag |= ~(CS8);
 	raw.c_oflag &= ~(OPOST);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw)==-1) die("tcsetattr");
 }
 
 
-int main(){
-	enableRawMode();
+int main() {
+  enableRawMode();
 
-	char c;
-	while(read(STDIN_FILENO, &c, 1)==1 && c != 'q')
-	{
-		if(iscntrl(c))
-			printf("%d\r\n", c);
-		else
-			printf("%d ('%c')\r\n",c,c);	
-	}
-		
+  while (1) {
+    char c = '\0';
+    if(read(STDIN_FILENO, &c, 1)==-1 && errno != EAGAIN) die("read"); 
 
-	return 0;
+    if (iscntrl(c))
+      printf("%d\r\n", c);
+    else 
+      printf("%d ('%c')\r\n", c, c);
+
+    if (c == 'q') break;
+  }
+  return 0;
 }
