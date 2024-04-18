7a8745438e886c2d0efc425274994c3f5acd38a5
Mohamed Hajji <mohamed.hajji.med@gmail.com>
2024-04-18 07:23:40 +0100

    12th gettin window size (the hrd way)

--- tile.c
+++ tile.c
@@ -80,10 +80,31 @@ void editorRefreshScreen(){
 }
 
 
+int getCusorPosition(int *rows, int *cols){
+	if(write(STDOUT_FILENO, "\x1b[6n",4)!=4) return -1; // query the cursor position
+
+	char buf[32];
+	unsigned int i = 0;
+
+	while(i<sizeof(buf)-1){
+		if(read(STDIN_FILENO, &buf[i], 1)!=1) break;
+		if(buf[i]=='r') break;
+		i++;
+	}
+
+	buf[i] = '\0';
+	if(buf[0]!='\x1b' || buf[1]!='[') return -1;
+	if(sscanf(&buf[2],"%d;%d",rows, cols) != 2) return -1;
+	
+	return 0;
+}
+
 int getWindowSize(int *rows, int *cols){
 	struct winsize ws;
-	if(((ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws))==-1) | (ws.ws_col == 0))
-		return -1;
+	if((ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)==-1) || (ws.ws_col == 0)){
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12)!=12) return -1;
+		return getCusorPosition(rows, cols);
+	}
 	else{
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
@@ -106,3 +127,4 @@ int main() {
   }
   return 0;
 }
+	
