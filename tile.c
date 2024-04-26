b9b63b67140d4cd0cb6ffe6aa2ddd56b5830de7a
Mohamed Hajji <mohamed.hajji.med@gmail.com>
2024-04-26 20:05:25 +0100

    23rd pageUp & pageDown keys update

--- tile.c
+++ tile.c
@@ -14,7 +14,9 @@ enum editorKey{
 	ARROW_LEFT = 1000,
 	ARROW_UP,
 	ARROW_RIGHT,
-	ARROW_DOWN
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 struct editorConfig{
@@ -87,15 +89,29 @@ int editorReadKey(){
 		if(read(STDIN_FILENO, &seq[1], 1)!=1) return '\x1b';
 
 		if(seq[0] == '['){
-			switch(seq[1]){
+			if(seq[1]>='0' && seq[1]<='9'){
+				if(read(STDIN_FILENO,&seq[2],1)!=1) return '\x1b';
+				if(seq[2]=='~'){
+					switch(seq[1]){
+						case '5':
+							return PAGE_UP;
+						case '6':
+							return PAGE_DOWN;
+					}
+				}
+			}
+			else{
+				switch(seq[1]){
 				case 'A': return ARROW_UP;
 				case 'B': return ARROW_DOWN;
 				case 'C': return ARROW_RIGHT;
 				case 'D': return ARROW_LEFT;
+					
+				}
 			}
+			
 		}
 		return '\x1b';
-
 	}
 	else return c;
 }
@@ -103,16 +119,16 @@ int editorReadKey(){
 void editorMoveCursor(int key){
 	switch(key){
 		case ARROW_UP:
-			E.cy--;
+			if(E.cy>0) E.cy--;	
 			break;
 		case ARROW_LEFT:
-			E.cx--;
+			if(E.cx>0) E.cx--;
 			break;
 		case ARROW_RIGHT:
-			E.cx++;
+			if(E.cx<E.screencols-1) E.cx++;
 			break;
 		case ARROW_DOWN:
-			E.cy++;
+			if(E.cy<E.screenrows-1) E.cy++;
 			break;
 	}
 }
@@ -125,6 +141,15 @@ void editorProcessKeypress(){
 			write(STDOUT_FILENO,"\x1b[H",3);
 			exit(0);
 			break;
+		case PAGE_UP:
+		case PAGE_DOWN:
+		{
+			int times = E.screenrows;
+			while(times--){
+				editorMoveCursor((c==PAGE_UP) ? ARROW_UP : ARROW_DOWN);
+			}
+		}
+		break;
 		case ARROW_UP:
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
