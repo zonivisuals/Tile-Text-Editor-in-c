e5f2faa5e0fe3e0d75ac8c85f4dfeaf806608d9a
Mohamed Hajji <mohamed.hajji.med@gmail.com>
2024-04-26 16:59:29 +0100

    15th hide cursor when repainting

--- tile.c
+++ tile.c
@@ -97,11 +97,15 @@ void editorDrawRows(struct abuf *ab){
 void editorRefreshScreen(){
 	struct abuf ab = ABUF_INIT;
 	
+	abAppend(&ab,"\x1b[?25l",6);
 	abAppend(&ab, "\x1b[2J", 4);
 	abAppend(&ab,"\x1b[H",3); //reposition the cursor at top-left corner
-	editorDrawRows(&ab);
-	abAppend(&ab,"\x1b[H",3);
 	
+	editorDrawRows(&ab);
+	
+	abAppend(&ab,"\x1b[H",3);
+	abAppend(&ab,"\x1b[?25h",6);
+
 	write(STDOUT_FILENO, ab.data, ab.len);
   	freeAb(&ab);
 }
