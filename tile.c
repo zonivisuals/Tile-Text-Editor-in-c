28a841bc4e8a6288d5111c52ee9f809614e62f5f
zoni <mohamed.hajji.med@gmail.com>
2024-05-05 22:25:59 +0100

    32nd snapping cursor to the end of line

--- tile.c
+++ tile.c
@@ -175,6 +175,9 @@ void editorMoveCursor(int key) {
       }
       break;
   }
+  row = (E.cy >= E.numRows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row -> size: 0;
+  if(E.cx > rowlen) E.cx = rowlen;
 }
 
 void editorProcessKeypress(){
